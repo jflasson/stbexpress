60c479f57723c2fefc42b1d16b6c7736bcae456e
jflasson <jforsslasson@gmail.com>
2015-08-03 01:42:48 +0200

    create new cities

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+//at 45  min
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({ extended: false });
+
+var cities = {
+	'Lotopia': 'Desc1',
+	'Caspiana': 'Desc2',
+	'Indigo': 'Desc3'
+};
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/cities', function(request, response){
-	var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-	response.json(cities);
+	response.json(Object.keys(cities));
+});
+
+app.post('/cities', urlencode, function(request, response){
+	var newCity = request.body;
+	cities[newCity.name] = newCity.description;
+	response.status(201).json(newCity.name);
 });
 
 module.exports = app;
